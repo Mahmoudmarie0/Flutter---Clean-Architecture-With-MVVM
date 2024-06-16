4913eea0e9954b2851608c7f5d64793dcf5ce879
Mahmoud Marie <126269794+Mahmoudmarie0@users.noreply.github.com>
2024-06-16 15:05:29 +0300

    style manager added

---
+++ lib/presentation/styles_manager.dart
@@ -0,0 +1,46 @@
+import 'package:clean_architecture_with_mvvm/presentation/font_manager.dart';
+import 'package:flutter/material.dart';
+
+TextStyle _getTextStyle(
+    double fontSize, String fontFamily, FontWeight fontWeight, Color color) {
+  return TextStyle(
+      fontSize: fontSize,
+      fontFamily: fontFamily,
+      color: color,
+      fontWeight: fontWeight);
+}
+
+//regular style
+TextStyle getRegularStyle(
+    {double fontSize = FontSizeManager.s12, required Color color}) {
+  return _getTextStyle(
+      fontSize, FontConstants.fontFamily, FontWeightManager.regular, color);
+}
+
+//light text style
+TextStyle getLightStyle(
+    {double fontSize = FontSizeManager.s12, required Color color}) {
+  return _getTextStyle(
+      fontSize, FontConstants.fontFamily, FontWeightManager.light, color);
+}
+
+//bold text style
+TextStyle getBoldStyle(
+    {double fontSize = FontSizeManager.s12, required Color color}) {
+  return _getTextStyle(
+      fontSize, FontConstants.fontFamily, FontWeightManager.bold, color);
+}
+
+//Semibold text style
+TextStyle getSemiBoldStyle(
+    {double fontSize = FontSizeManager.s12, required Color color}) {
+  return _getTextStyle(
+      fontSize, FontConstants.fontFamily, FontWeightManager.semiBold, color);
+}
+
+//medium text style
+TextStyle getMediumStyle(
+    {double fontSize = FontSizeManager.s12, required Color color}) {
+  return _getTextStyle(
+      fontSize, FontConstants.fontFamily, FontWeightManager.medium, color);
+}
