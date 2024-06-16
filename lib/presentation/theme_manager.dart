3bfff318bbd46adb418ec73b8d1aa9bbb81856be
Mahmoud Marie <126269794+Mahmoudmarie0@users.noreply.github.com>
2024-06-16 15:53:24 +0300

    Add Card,AppBar Theme in ThemeData

--- lib/presentation/theme_manager.dart
+++ lib/presentation/theme_manager.dart
@@ -1,15 +1,35 @@
 import 'package:clean_architecture_with_mvvm/presentation/color_manager.dart';
+import 'package:clean_architecture_with_mvvm/presentation/font_manager.dart';
+import 'package:clean_architecture_with_mvvm/presentation/styles_manager.dart';
+import 'package:clean_architecture_with_mvvm/presentation/values_manager.dart';
 import 'package:flutter/material.dart';
 
 ThemeData getApplicationTheme() {
   return ThemeData(
-  //main colors of the app
+    //main colors of the app
     primaryColor: ColorManager.primary,
     primaryColorLight: ColorManager.primaryOpacity70,
     primaryColorDark: ColorManager.darkPrimary,
-    disabledColor:  ColorManager.grey1, //will be used incase of disabled buttons for example
+    disabledColor: ColorManager
+        .grey1, //will be used incase of disabled buttons for example
+    splashColor: ColorManager.primaryOpacity70, //ripple effect color
     hintColor: ColorManager.grey,
 
+    //Card view Theme
+    cardTheme: CardTheme(
+      color: ColorManager.white,
+      shadowColor: ColorManager.grey,
+      elevation: AppSize.s4,
+    ),
 
+    //AppBar Theme
+    appBarTheme: AppBarTheme(
+      centerTitle: true,
+      color: ColorManager.primary,
+      elevation: AppSize.s4,
+      shadowColor: ColorManager.primaryOpacity70,
+      titleTextStyle: getRegularStyle(
+          color: ColorManager.white, fontSize: FontSizeManager.s16),
+    ),
   );
 }
