baf84e4f362d8a7105b4d2fe9140f57b8d69961c
Mahmoud Marie <126269794+Mahmoudmarie0@users.noreply.github.com>
2024-06-16 15:58:57 +0300

    Add Button,ElevatedButton Theme in ThemeData

--- lib/presentation/theme_manager.dart
+++ lib/presentation/theme_manager.dart
@@ -6,30 +6,47 @@ import 'package:flutter/material.dart';
 
 ThemeData getApplicationTheme() {
   return ThemeData(
-    //main colors of the app
-    primaryColor: ColorManager.primary,
-    primaryColorLight: ColorManager.primaryOpacity70,
-    primaryColorDark: ColorManager.darkPrimary,
-    disabledColor: ColorManager
-        .grey1, //will be used incase of disabled buttons for example
-    splashColor: ColorManager.primaryOpacity70, //ripple effect color
-    hintColor: ColorManager.grey,
+      //main colors of the app
+      primaryColor: ColorManager.primary,
+      primaryColorLight: ColorManager.primaryOpacity70,
+      primaryColorDark: ColorManager.darkPrimary,
+      disabledColor: ColorManager
+          .grey1, //will be used incase of disabled buttons for example
+      splashColor: ColorManager.primaryOpacity70, //ripple effect color
+      hintColor: ColorManager.grey,
 
-    //Card view Theme
-    cardTheme: CardTheme(
-      color: ColorManager.white,
-      shadowColor: ColorManager.grey,
-      elevation: AppSize.s4,
-    ),
+      //Card view Theme
+      cardTheme: CardTheme(
+        color: ColorManager.white,
+        shadowColor: ColorManager.grey,
+        elevation: AppSize.s4,
+      ),
 
-    //AppBar Theme
-    appBarTheme: AppBarTheme(
-      centerTitle: true,
-      color: ColorManager.primary,
-      elevation: AppSize.s4,
-      shadowColor: ColorManager.primaryOpacity70,
-      titleTextStyle: getRegularStyle(
-          color: ColorManager.white, fontSize: FontSizeManager.s16),
-    ),
-  );
+      //AppBar Theme
+      appBarTheme: AppBarTheme(
+        centerTitle: true,
+        color: ColorManager.primary,
+        elevation: AppSize.s4,
+        shadowColor: ColorManager.primaryOpacity70,
+        titleTextStyle: getRegularStyle(
+            color: ColorManager.white, fontSize: FontSizeManager.s16),
+      ),
+
+      //Button Theme
+      buttonTheme: ButtonThemeData(
+          shape: const StadiumBorder(),
+          disabledColor: ColorManager.grey1,
+          buttonColor: ColorManager.primary,
+          splashColor: ColorManager.primaryOpacity70),
+
+      //Elevated Button Theme
+      elevatedButtonTheme: ElevatedButtonThemeData(
+        style: ElevatedButton.styleFrom(
+          textStyle: getRegularStyle(color: ColorManager.white),
+          backgroundColor: ColorManager.primary,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(AppSize.s12),
+          ),
+        ),
+      ));
 }
