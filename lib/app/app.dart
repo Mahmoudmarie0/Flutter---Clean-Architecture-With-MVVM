7f049584675124df7e13f2269b79dd1c789388f2
Mahmoud Marie <126269794+Mahmoudmarie0@users.noreply.github.com>
2024-06-17 16:35:07 +0300

    Splash Screen

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,3 +1,4 @@
+import 'package:clean_architecture_with_mvvm/presentation/resources/routes_manager.dart';
 import 'package:clean_architecture_with_mvvm/presentation/resources/theme_manager.dart';
 import 'package:flutter/material.dart';
 
@@ -15,6 +16,9 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      onGenerateRoute: RouteGenerator.getRoute,
+      initialRoute: Routes.splashRoute,
       theme: getApplicationTheme(),
     );
   }
--- lib/presentation/splash/splash.dart
+++ lib/presentation/splash/splash.dart
@@ -1,3 +1,8 @@
+import 'dart:async';
+
+import 'package:clean_architecture_with_mvvm/presentation/resources/assets_manager.dart';
+import 'package:clean_architecture_with_mvvm/presentation/resources/color_manager.dart';
+import 'package:clean_architecture_with_mvvm/presentation/resources/routes_manager.dart';
 import 'package:flutter/material.dart';
 
 class SplashView extends StatefulWidget {
@@ -8,8 +13,36 @@ class SplashView extends StatefulWidget {
 }
 
 class _SplashViewState extends State<SplashView> {
+  Timer? _timer;
+  _startDelay() {
+    _timer = Timer(const Duration(seconds: 2), _goNext);
+  }
+
+  _goNext() {
+    Navigator.pushReplacementNamed(context, Routes.onBoardingRoute);
+  }
+
+  //to cancel timer after finish splash
+  @override
+  void dispose() {
+    _timer?.cancel();
+    super.dispose();
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _startDelay();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: ColorManager.primary,
+      body: const Center(
+          child: Image(
+        image: AssetImage(ImageAssets.splashLogo),
+      )),
+    );
   }
 }
