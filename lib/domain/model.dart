4a84611cc09dd46167ac318ec6adba97b8484513
Mahmoud Marie <126269794+Mahmoudmarie0@users.noreply.github.com>
2024-06-23 16:52:47 +0300

    model Adedd

--- lib/domain/model.dart
+++ lib/domain/model.dart
@@ -5,3 +5,28 @@ class SliderObject {
   SliderObject(
        this.title,  this.subTitle,  this.image);
 }
+
+
+
+class Customer {
+  String id;
+  String name;
+  int numOfNotification;
+  Customer(this.id, this.name, this.numOfNotification);
+}
+
+
+class Contacts {
+  String phone;
+  String link;
+  String email;
+  Contacts(this.phone, this.link, this.email);
+}
+
+
+
+class Authentication {
+  Customer customer;
+  Contacts contacts;
+  Authentication(this.customer, this.contacts);
+}
