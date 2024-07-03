ec971f3d175b2339f68513fc082324d3bbfd1396
Mahmoud Marie <126269794+Mahmoudmarie0@users.noreply.github.com>
2024-07-03 14:23:07 +0300

    Dependency Injection Added continue

--- lib/app/di.dart
+++ lib/app/di.dart
@@ -1,6 +1,15 @@
 import 'package:clean_architecture_with_mvvm/app/app_prefs.dart';
+import 'package:clean_architecture_with_mvvm/data/data_source/remote_data_source.dart';
+import 'package:clean_architecture_with_mvvm/data/network/app_api.dart';
+import 'package:clean_architecture_with_mvvm/data/repository/repositery.impl.dart';
+import 'package:clean_architecture_with_mvvm/domain/repository/repository.dart';
+import 'package:dio/src/dio.dart';
 import 'package:get_it/get_it.dart';
+import 'package:internet_connection_checker/internet_connection_checker.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+
+import '../data/network/dio_factory.dart';
+import '../data/network/network_info.dart';
 
 final instance = GetIt.instance;
 
@@ -13,4 +22,22 @@ Future<void> initAppModule() async {
   //app prefs instance/lazy singleton her
   instance.registerLazySingleton<AppPrefrences>(() => AppPrefrences(instance()));
   // TODO: implement init
+
+  //network info
+  instance.registerLazySingleton<NewtworkInfo>(() => NetworkInfoImpl(InternetConnectionChecker()));
+
+  //dio factory
+  instance.registerLazySingleton<DioFactory>(() => DioFactory(instance()));
+
+  //app service client
+
+  final dio =  instance<DioFactory>().getDio();
+  instance.registerLazySingleton<AppServiceClient>(() => AppServiceClient(dio as Dio));
+
+  //remote data source
+  instance.registerLazySingleton<RemoteDataSource>(() => RemoteDataSourceImplementer(instance()));
+
+  //repository
+  instance.registerLazySingleton<Repository>(() => RepositoryImpl(instance(),instance()));
+
 }
